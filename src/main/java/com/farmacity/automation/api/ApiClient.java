57cfc7734330274016c71a02ccbd89d967fdb5a0
Yailin Valdivia <yailin.valdivia@globant.com>
2026-03-23 16:53:48 -0300

    Remove the final variable assignment

--- src/main/java/com/farmacity/automation/api/ApiClient.java
+++ src/main/java/com/farmacity/automation/api/ApiClient.java
@@ -18,7 +18,7 @@ import java.util.Map;
  */
 public class ApiClient {
 
-    private final RequestSpecification spec;
+    private RequestSpecification spec;
 
     public ApiClient() {
         spec = RestAssured
